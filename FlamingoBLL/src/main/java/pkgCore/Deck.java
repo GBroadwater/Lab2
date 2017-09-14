d3cd894038959d78ecf6f0474c706c873735e6da
geoff <geoff@128.4.125.214>
2017-09-14 14:06:39 -0400

    Changed variable name in class 'Deck'

--- FlamingoBLL/src/main/java/pkgCore/Deck.java
+++ FlamingoBLL/src/main/java/pkgCore/Deck.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 
 public class Deck {
 
-	private ArrayList<Card> cards;
+	private ArrayList<Card> deckCards;
 	
 	public Deck(){
 		for (eRank eRank: eRank.values()) {
 			for (eSuit eSuit: eSuit.values()) {
-				cards.add(new Card(eSuit, eRank));
+				deckCards.add(new Card(eSuit, eRank));
 			}
 		}
 	}
 	public Card Draw() {
-		return (cards.remove(0));
+		return (deckCards.remove(0));
 		
 	}
 
